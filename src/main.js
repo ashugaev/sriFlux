d2196bfde33ab381e7f854bcb58e8360ca10901a
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2018-10-10 19:41:30 +0300

    connected pointers

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
 import './index.pug'
 import './index.scss'
-
-// window.addEventListener('load', ()=> {
-//     let titleBox, titleIn
-//     const titles = document.querySelectorAll(".eventItem__title")
-//     titles.forEach((item, i) => {
-//         titleBox = item.offsetHeight;
-//         titleIn = item.querySelector('span').offsetHeight
-//         const titleText = item.querySelector('span').textContent
-//         console.log(titleBox, titleIn)
-//         if(titleBox < titleIn) item.querySelector('span').textContent += '...'
-//         console.log(titleText.split('\n').length)
-//     })
-// })
+import './js/pointerEvents'
