f6533e00c8b1270cccd416df432e5cd59de63fe0
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2018-10-15 00:10:27 +0300

    init function

--- src/pages/cameras/index.js
+++ src/pages/cameras/index.js
@@ -1,12 +1,20 @@
 import "./index.scss";
 import { createVideoNodes, startVideo, initCanvas } from "./init";
+import { initPopup } from "./openVideo";
+import { audioAnalizeInit } from "./audioAnalize";
+import { setControllers } from "./controllers";
+
+// const streamingServer = 'http://19fe4bb6.ngrok.io'
+const streamingServer = 'http://localhost:9191'
 
 const urls = [
-  "http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8",
-  "http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8",
-  "http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8",
-  "http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8"
+  `${streamingServer}/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8`,
+  `${streamingServer}/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8`,
+  `${streamingServer}/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8`,
+  `${streamingServer}/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8`
 ];
+
+// export let activeVideoSelector;
 
 function initVideos() {
   createVideoNodes(urls);
@@ -14,8 +22,10 @@ function initVideos() {
   urls.forEach((url, i) => {
     const selector = document.querySelector(`#video-${i}`);
     startVideo(selector, url);
-    initCanvas(selector, i);
+    audioAnalizeInit(selector, i);
+    setControllers(selector);
   });
+  initPopup();
 }
 
 initVideos();
